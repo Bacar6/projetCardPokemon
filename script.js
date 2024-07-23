813a9924e4ae00d7d222dad8109f5e30442db7d1
gala <andhoumani13003.g@gmail.com>
2024-07-23 10:16:27 +0200

    Ajout du fichier js et du script js

---
+++ script.js
@@ -0,0 +1,24 @@
+const tagImg = document.getElementById("tagImg");
+const namePokemon = document.getElementById("namePokemon");
+const buttonPrev = document.getElementById("buttonPrev");
+const buttonNext = document.getElementById("buttonNext");
+
+displayImagePokemon = async () => {
+  let randomNumber = Math.ceil(Math.random() * 898) + 1;
+
+  const urlPokemon = `https://pokebuildapi.fr/api/v1/pokemon/${randomNumber}`;
+
+  let dataPokemon = await fetch(urlPokemon);
+  console.log(urlPokemon);
+
+  let response = await dataPokemon.json();
+  console.log(response);
+
+  tagImg.src = response.image;
+  namePokemon.innerHTML = `<p><stong>${response.name}</stong></p>`;
+};
+
+displayImagePokemon();
+
+buttonPrev.addEventListener("click", displayImagePokemon);
+buttonNext.addEventListener("click", displayImagePokemon);
